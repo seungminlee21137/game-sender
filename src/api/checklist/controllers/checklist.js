7b5609e8236902fcdbd7debbea23ac4bf39283d2
seungminlee <lee21137@mangot5.com>
2022-08-12 18:50:15 +0900

    add server type

--- src/api/checklist/controllers/checklist.js
+++ src/api/checklist/controllers/checklist.js
@@ -1,6 +1,12 @@
 "use strict";
 
+const fs = require("fs");
+const { promises } = require("fs");
+const util = require("util");
+
+// const log_file = fs.createWriteStream(__dirname + "/debug.log", { flags: "w" });
 const { tunnelSSH } = require("../../../../utils/connect-helper");
+
 const sshconfig = {
   host: "10.100.2.4",
   username: process.env.ssh_username, // env.username
@@ -8,6 +14,38 @@ const sshconfig = {
 };
 
 module.exports = {
+  /**
+   * 요청서버의 war 정보를 가져온다.
+   * -rw-rw-r--. 1 happytuk 52277210 2022-08-11 16:29 /usr/local/tomcat7/webapps/ROOT.war
+   *
+   */
+  async warcheck(ctx) {
+    const { serverip = "" } = ctx.params;
+
+    if (!serverip) {
+      return { ip: "" };
+    }
+    // connet-server
+    sshconfig.host = serverip;
+    const result = {};
+
+    const _server = await tunnelSSH(sshconfig);
+
+    if (_server) {
+      const commands = await _server.exec(
+        "ls /usr/local/tomcat7/webapps/ROOT.war --time-style=long-iso -lrg"
+      );
+
+      const command = commands.split(" ");
+      result.ip = serverip;
+      result.size = command[3];
+      result.releaseDate = `${command[4]} ${command[5]}`;
+
+      _server.close();
+    }
+
+    return result;
+  },
   async deployWeb() {
     const webservers = strapi.config.get("server.server_live.jcgf");
     const result = {};
@@ -60,5 +98,84 @@ module.exports = {
     }
 
     return result;
+  },
+  async serverlogTail(ctx) {
+    const { ip = "" } = ctx.params;
+
+    if (!ip) {
+      return { result: "invaild_ip_address" };
+    }
+
+    const webServers = strapi.config.get("server.server_live.jcgf");
+    const apiServers = strapi.config.get("server.server_live.happycode");
+
+    let isWebSever = false;
+    let isApiSever = false;
+
+    if (webServers.indexOf(ip) > -1) {
+      isWebSever = true;
+    } else if (apiServers.indexOf(ip) > -1) {
+      isApiSever = true;
+    }
+
+    if (!isWebSever && !isApiSever) {
+      return { result: `${ip}_not_server_ip` };
+    }
+
+    sshconfig.host = ip;
+
+    // 파일로드
+    async function loadMonoCounter(filepath) {
+      const data = await promises.readFile(filepath, "binary");
+      return Buffer.from(data);
+    }
+    // 파일 체크
+    const loadFileExists = async (path) =>
+      !!(await fs.promises.stat(path).catch((e) => false));
+
+    // D://debug_{ip}.txt
+    const pathAndFile = `D://debug_${ip}.txt`;
+    const isExigst = await loadFileExists(pathAndFile);
+    console.log(`isExigst`, isExigst);
+
+    let log_file = "";
+    let tailinfo = "";
+    if (isExigst) {
+      // log_file = await loadMonoCounter(pathAndFile);
+      fs.unlink(pathAndFile, function (err) {
+        if (err) throw err;
+        console.log(pathAndFile + "File deleted!");
+      });
+      log_file = fs.createWriteStream(pathAndFile, { flags: "w" });
+    } else {
+      log_file = fs.createWriteStream(pathAndFile, { flags: "w" });
+    }
+
+    const _server = await tunnelSSH(sshconfig);
+
+    if (_server) {
+      try {
+        _server
+          .spawn("tail -f /usr/local/tomcat7/logs/catalina.out")
+          .then((socket) => {
+            socket.on("data", (loginfo) => {
+              const taillog = loginfo.toString("utf-8");
+              console.log(taillog);
+              // save to logfile
+              console.log = (taillog) => {
+                log_file.write(util.format(taillog) + "\n");
+              };
+            });
+          });
+      } catch (err) {
+        if (_server) {
+          _server.close();
+        }
+      }
+
+      return { result: pathAndFile, log: log_file };
+    } else {
+      return { result: "connect.fail" };
+    }
   },
 };
--- src/api/checklist/routes/checklist.js
+++ src/api/checklist/routes/checklist.js
@@ -4,6 +4,15 @@ module.exports = {
   routes: [
     {
       method: "GET",
+      path: "/checklist/war/:serverip",
+      handler: "checklist.warcheck",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: "GET",
       path: "/checklist/jcgf",
       handler: "checklist.deployWeb",
       config: {
@@ -15,6 +24,15 @@ module.exports = {
       method: "GET",
       path: "/checklist/happycode",
       handler: "checklist.deployApi",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: "GET",
+      path: "/checklist/logtail/:ip",
+      handler: "checklist.serverlogTail",
       config: {
         policies: [],
         middlewares: [],
